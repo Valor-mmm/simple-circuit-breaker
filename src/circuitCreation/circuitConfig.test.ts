12fd7d75a1eeb01a91f898fab0e8026eb32b7d2f
Stefan Hauser <stefan.hauser@tutanota.com>
2020-09-05 15:29:07 +0200

    feat(error counter reset): writing and fixing tests

--- src/circuitCreation/circuitConfig.test.ts
+++ src/circuitCreation/circuitConfig.test.ts
@@ -1,49 +1,98 @@
-import { mergeConfigWithDefaults } from './circuitConfig';
+import {
+  CircuitConfig,
+  mergeConfigs,
+  mergeConfigWithDefaults,
+  PartialCircuitConfig,
+} from './circuitConfig';
+import { createTestConfig } from '../utils/test.utils';
 
-describe('Test mergeConfigWithDefaults', () => {
-  it('should overwrite the default config with a given failureThreshold prop', () => {
-    expect(mergeConfigWithDefaults({ failureThreshold: 60 })).toEqual({
-      failureThreshold: 60,
-      successThreshold: expect.anything(),
+describe('Test circuitConfig', () => {
+  describe('Test mergeConfigWithDefaults', () => {
+    const anythingConfig: CircuitConfig = {
+      failureCounterResetInterval: expect.anything(),
       timeout: expect.anything(),
-    });
-  });
-
-  it('should overwrite the default config with a given successThreshold prop', () => {
-    expect(mergeConfigWithDefaults({ successThreshold: 60 })).toEqual({
-      failureThreshold: expect.anything(),
-      successThreshold: 60,
-      timeout: expect.anything(),
-    });
-  });
-
-  it('should overwrite the default config with a given timeout prop', () => {
-    expect(mergeConfigWithDefaults({ timeout: 60 })).toEqual({
       failureThreshold: expect.anything(),
       successThreshold: expect.anything(),
-      timeout: 60,
-    });
-  });
+    };
 
-  it('should overwrite all default options if needed', () => {
-    expect(
-      mergeConfigWithDefaults({
-        timeout: 60,
-        successThreshold: 60,
+    it('should overwrite the default config with a given failureThreshold prop', () => {
+      expect(mergeConfigWithDefaults({ failureThreshold: 60 })).toEqual({
+        ...anythingConfig,
         failureThreshold: 60,
-      })
-    ).toEqual({
-      failureThreshold: 60,
-      successThreshold: 60,
-      timeout: 60,
+      });
+    });
+
+    it('should overwrite the default config with a given successThreshold prop', () => {
+      expect(mergeConfigWithDefaults({ successThreshold: 60 })).toEqual({
+        ...anythingConfig,
+        successThreshold: 60,
+      });
+    });
+
+    it('should overwrite the default config with a given timeout prop', () => {
+      expect(mergeConfigWithDefaults({ timeout: 60 })).toEqual({
+        ...anythingConfig,
+        timeout: 60,
+      });
+    });
+
+    it('should overwrite all default options if needed', () => {
+      expect(
+        mergeConfigWithDefaults({
+          timeout: 60,
+          successThreshold: 60,
+          failureThreshold: 60,
+          failureCounterResetInterval: 90,
+        })
+      ).toEqual({
+        failureThreshold: 60,
+        successThreshold: 60,
+        timeout: 60,
+        failureCounterResetInterval: 90,
+      });
+    });
+
+    it('should use default config if no overwrite object is provided', () => {
+      expect(mergeConfigWithDefaults()).toEqual({
+        failureThreshold: 10,
+        successThreshold: 2,
+        timeout: 300,
+        failureCounterResetInterval: 5000,
+      });
     });
   });
 
-  it('should use default config if no overwrite object is provided', () => {
-    expect(mergeConfigWithDefaults()).toEqual({
-      failureThreshold: 10,
-      successThreshold: 2,
-      timeout: 300,
+  describe('Test mergeConfigs', () => {
+    it('should overwrite existing properties of a current configuration', () => {
+      const config = createTestConfig({ failureThreshold: 1 });
+      const newProperties: PartialCircuitConfig = { failureThreshold: 2 };
+
+      expect(mergeConfigs(newProperties, config)).toEqual({
+        ...config,
+        failureThreshold: 2,
+      });
+    });
+
+    it('should overwrite all properties of a current configuration', () => {
+      const config = createTestConfig({
+        failureThreshold: 1,
+        timeout: 100,
+        successThreshold: 1,
+        failureCounterResetInterval: 1,
+      });
+      const newProperties: PartialCircuitConfig = {
+        failureThreshold: 2,
+        timeout: 600,
+        successThreshold: 4,
+        failureCounterResetInterval: 3,
+      };
+
+      expect(mergeConfigs(newProperties, config)).toEqual({
+        failureThreshold: 2,
+        timeout: 600,
+        successThreshold: 4,
+        failureCounterResetInterval: 3,
+      });
     });
   });
 });
